5724f9392e3c97d6fbfe2ceb8796f17eee0fbcf4
olchangiagi <olchangiagi@gmail.com>
2026-08-13 13:24:56 +0900

    Feat: 데이터 획득 완료

--- dags/05_mysql_etl.py
+++ dags/05_mysql_etl.py
@@ -58,6 +58,13 @@ def _extract(**kwargs):
     return file_full_path
 
 def _transform(**kwargs):
+    # 1. extract가 전달한 내용을 XCOM을 통해 획득
+    ti = kwargs["ti"]
+    json_file_path = ti.xcom_pull(task_ids = "extract")
+    logging.info(f"전달한 데이터 파일 경로 {json_file_path}")
+    
+
+
     pass
 
 def _load(**kwargs):
