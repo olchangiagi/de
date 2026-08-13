f56902a8f9861a32f5a7bbb21c8a8482f9626433
olchangiagi <olchangiagi@gmail.com>
2026-08-13 10:14:36 +0900

    Feat: 골격 수정

--- dags/03_basics_context_jinja.py
+++ dags/03_basics_context_jinja.py
@@ -1,8 +1,7 @@
 '''
 - airflow 내부 정보 접근, 출력시 jinja 활용, 내부 정보 접근시 macro 활용
-- 콜백함수 내부는 kwargs를 인자를 통해 접근, 기타 일반적인 상황은 jinja를 이용하여 접근
+- 콜백함수 내부 kwargs를 인자를 통해 접근, 기타 일반적인 상황 jinja를 이용하여 접근
 '''
-
 # 1. 모듈
 from airflow import DAG
 from airflow.operators.python import PythonOperator
@@ -11,46 +10,45 @@ from datetime import datetime, timedelta
 import logging
 import pendulum
 
-# 1-1. 전역변수
+# 2. 전역변수
 KST = pendulum.timezone("Asia/Seoul")
 
-# 1-2. 콜백 함수
+# 4-1. 콜백함수
 def _print(**kwargs):
-    logging.info(f'ds 출력 {kwargs["ds"]}')
-    pass
+  logging.info(f'ds 출력 { kwargs["ds"] }')
+  pass
 
-# 2. DAG
-with DAG (
-    dag_id = "03_basics_context_jinja", 
-    description = "macro를 이용하여 context 접근, jinja를 통해 표현", 
-    default_args = {
-    "owner"             : "aic-de1-admin",
-    "retries"           : 1, 
-    "retry_delay"       : timedelta(minutes= 1), 
-    },
-    # 매일 오전 9시 00분에 스케줄 작동
-    schedule_interval = "0 9 * * *", # cron 표기 (분, 시, 일, 월, 주)
-    # 수행 시작 시간 서울 시간대 타임존 조정
-    start_date = pendulum.datetime(2026,6,29, tz = KST), 
-    catchup = False, 
-    tags = ["macro", "context", "jinja"]
-
+# 3. DAG
+with DAG( 
+  dag_id      = "03_basics_context_jinja",
+  description = "macro을 이용하여 context 접근, jinja를 통해 표현",
+  default_args= {
+    "owner"           : "aic-de1-admin",    
+    "retries"         : 1,
+    "retry_delay"     : timedelta(minutes=1)
+  },
+  # 매일 오전 9시 00분에 스케줄 작동
+  schedule_interval = "0 9 * * *", # cron 방식으로 표기 (분, 시, 일, 월, 주)
+  # 수행 시작 시간 서울 시간대 타임존 조정
+  start_date  = pendulum.datetime( 2026,6,29, tz=KST ),
+  catchup     = False,
+  tags        = ['macro', 'context', 'jinja']
 ) as dag:
 
-# 3. 오퍼레이터를 이용하여 task를 정의
-    t1 = BashOperator(
-        task_id = "jinja_used_task",
-        bash_command = "echo 'DAG의 t1 task 수행시간 {{ds}}, {{ti}}'"
-    )
-    t2 = BashOperator(
-        task_id = "jinja_macro_task",
-        # macro를 통해 준비된 함수 활용
-        bash_command = "echo 'DAG의 t1 task 일주일전 수행시간(임시) {{macros.ds_add(ds - 7)}}, {{macros.random()}}'"
-    )
-    t3 = PythonOperator(
-        task_id = "jinja_python_task",
-        python_callable = _print
-    )
+  # 4. 오퍼레이터를 이용하여 task를 정의
+  t1 = BashOperator(
+    task_id         = "jinja_used_task",
+    bash_command    = "echo 'DAG의 t1 task 수행시간 {{ ds }}, {{ ti }}' "
+  )
+  t2 = BashOperator(
+    task_id         = "jinja_macro_task",
+    # macro를 통해서 준비된 함수 활용
+    bash_command    = "echo 'DAG의 t1 task일주일전 수행시간(임시) {{ macros.ds_add(ds, -7) }}, 램덤 {{ macros.random() }}' "
+  )
+  t3 = PythonOperator(
+    task_id         = "jinja_python_task",
+    python_callable = _print
+  )
 
-# 4. 의존성(수행순서)
-t1 >> t2 >> t3
+  # 5. 의존성(수행순서)
+  t1 >> t2 >> t3
---
+++ dags/04_basics_branching.py
@@ -0,0 +1,56 @@
+'''
+- 파이썬 오퍼레이터와 연결된 콜백 함수 내부 연산의 결과로 조건부로 task를 선택하여 진행
+- 의존성 컨트롤, 조건부 task 수행 -> 브런치(가지치기)
+- 의존성 구성에서 여러 시나리오가 작성
+'''
+# 1. 모듈
+from airflow import DAG
+from airflow.operators.python import PythonOperator, BranchPythonOperator # 조건부 선택
+from airflow.operators.empty import EmptyOperator
+from airflow.utils.trigger_rule import TriggerRule # 성공/실패 등등 조건 설정
+from datetime import datetime, timedelta
+import logging
+import pendulum
+import random
+
+# 2. 전역변수
+KST = pendulum.timezone("Asia/Seoul")
+
+# 3. DAG
+with DAG( 
+  dag_id      = "04_basics_branching",
+  description = "분기 처리, 선택적 Task 구동",
+  default_args= {
+    "owner"           : "aic-de1-admin",    
+    "retries"         : 1,
+    "retry_delay"     : timedelta(minutes=1)
+  },
+  schedule_interval = "@daily", 
+  start_date  = pendulum.datetime( 2026,6,29, tz=KST ),
+  catchup     = False,
+  tags        = ['branch', 'trigger_rule']
+) as dag:
+
+  # 4. 오퍼레이터를 이용하여 task를 정의
+    task_start = EmptyOperator(
+        task_id = "start"
+    )
+    task_branch = BranchPythonOperator(
+        task_id = "branch"
+        python_callable = _branch_cb
+    )
+    task_process = PythonOperator(
+        task_id = "process"
+        python_callable = _process_cb
+    )
+    task_skip = EmptyOperator(
+        task_id = "skip"
+    )
+    task_end = EmptyOperator(
+        task_id = "end"
+    )
+
+  # 5. 의존성(수행순서)
+    task_start >> task_branch
+    task_branch >> task_process >> task_end
+    task_branch >> task_skip >> task_end
