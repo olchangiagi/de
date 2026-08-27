904e01860584c79a6a70462e23c900f2d42bdc10
olchangiagi <olchangiagi@gmail.com>
2026-08-27 09:52:09 +0900

    Feat: 인프라 수정

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -3,3 +3,18 @@ output "s3_bucket_name" {
   description = "s3 bucket name by airflow"
   value       = local.airflow_bucket_name
 }
+
+output "silver_glue_database_name" {
+  description = "s3 bucket name by airflow"
+  value       = local.airflow_bucket_name
+}
+
+output "glue_table_name" {
+  description = "s3 bucket name by airflow"
+  value       = local.airflow_bucket_name
+}
+
+output "silver_s3_bucket_name" {
+  description = "s3 bucket name by airflow"
+  value       = "s3://${var.silver_bucket_name}/silver/"
+}
