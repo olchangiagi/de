5bf1b0c69d58b684bf1b33a58ba6b903aef8e8e9
olchangiagi <olchangiagi@gmail.com>
2026-08-27 15:09:31 +0900

    Feat: s3 경로 조정

--- dags/11_athena_partition_gold_data.py
+++ dags/11_athena_partition_gold_data.py
@@ -34,7 +34,7 @@ TARGET_DAY   = "27"   #"{{ dag_run.conf.get('target_date', ds)[8:10] }}"
 
 # 매일 1개의 데이터셋 구성 -> 파티션 사용 권장
 # s3://버킷/gold/daily_report/year=2026/month=08/day=27/
-GOLD_PARTITION_PREFIX = f"{GOLD_PREFIX}/year={TARGET_YEAR}/month={TARGET_MONTH}/day={TARGET_DAY}/"
+GOLD_PARTITION_PREFIX = f"{GOLD_PREFIX}year={TARGET_YEAR}/month={TARGET_MONTH}/day={TARGET_DAY}/"
 
 # 3. DAG 정의
 with DAG(  
@@ -87,11 +87,77 @@ with DAG(
         output_location = QUERY_RESULT_S3
     )
 
-
-
     # 4-2. 동일 날짜에 대해서 중복적 실행하는 컨셉이라면 -> 허용(1일 1회인데 허용), 하루에 여러번 수행 컨셉(누적)
+    t2_drop_partition = AthenaOperator(
+        task_id = "drop_partition",
+        # sql
+        query = f'''
+            ALTER TABLE {GOLD_TABLE_NAME}
+            DROP if exists Partition (
+                year = '{TARGET_YEAR}',
+                month = '{TARGET_MONTH}',
+                day = '{TARGET_DAY}'
+            )
+        ''',
+        # 접속 및 디비 정보
+        aws_conn_id = AWS_CONN_ID,
+        database    = DATABASE_NAME,
+        output_location = QUERY_RESULT_S3    
+    )
 
     # 4-3. 누적 교체하는 관점 -> 데이터 삭제 처리 필요
+    t3_delete_gold_s3  = S3DeleteObjectsOperator(
+        task_id = "delete_gold_s3",
+        bucket  = BUCKET_NAME,
+        prefix  = GOLD_PARTITION_PREFIX,
+        aws_conn_id = AWS_CONN_ID
+    )
+
+    # 4-4. 당일 전체 데이터에 대한(파티션 수행) 데이터 insert 처리
+    t4_insert_gold_table = AthenaOperator(
+        task_id = "insert_gold_table",
+        # sql
+        query = f'''
+            insert into {GOLD_TABLE_NAME} 
+            select
+                DATE('{TARGET_DATE}') AS report_date,
+                domain,
+                event_type,
+                COALESCE( service.name,'unknown' ) AS service_name,
+                COUNT(*) AS total_count,
+                COUNT(response.status_code) AS response_count,
+                COUNT_IF( response.status_code >= 200 AND response.status_code < 400 ) AS success_count,
+                COUNT_IF( response.status_code >= 400 ) AS error_count,
+                CASE
+                    WHEN COUNT(response.status_code) = 0 THEN 0
+                    ELSE ROUND( 100.0 * COUNT_IF(response.status_code >= 400) / COUNT(response.status_code), 2 )
+                END
+                    AS error_rate_pct,
+                ROUND( AVG( CAST( response.latency_ms AS DOUBLE ) ), 2 ) AS avg_latency_ms,
+                MIN(response.latency_ms) AS min_latency_ms,
+                APPROX_PERCENTILE( response.latency_ms, 0.95 ) AS p95_latency_ms,
+                MAX(response.latency_ms) AS max_latency_ms,
+                COALESCE( SUM(request.request_bytes), 0 ) AS total_request_bytes,
+                COALESCE( SUM(response.response_bytes), 0 ) AS total_response_bytes,
+                '{TARGET_YEAR}' as year,
+                '{TARGET_MONTH}' as month,
+                '{TARGET_DAY}' as day
+            from {SILVER_TABLE_NAME}
+            where 
+                year='{TARGET_YEAR}' and
+                month='{TARGET_MONTH}' and
+                day='{TARGET_DAY}'
+            group by
+                domain,
+                event_type,
+                service.name
+        ''',
+        aws_conn_id = AWS_CONN_ID,
+        database    = DATABASE_NAME,
+        output_location = QUERY_RESULT_S3,
+        # 워크그룹의 저장 위치가 더 우선순위가 됨
+        # workgroup   = "de-ai-08-loggen-analysis"
+    )
 
     # 4-1. 기존 CTAS Gold 테이블 삭제
     # t1_drop_gold_table = AthenaOperator(
@@ -163,4 +229,4 @@ with DAG(
     # )
 
     # 5. 의존성 구성 (수행 순서 >> )
-    t1_create_gold_table  # >> t2_delete_gold_s3 >> t3_create_gold_table_with_ctas
+    t1_create_gold_table  >> t2_drop_partition >> t3_delete_gold_s3 >> t4_insert_gold_table
