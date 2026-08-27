a0d1876f125b530c48d1c9a0e51f721e9d1b33d4
olchangiagi <olchangiagi@gmail.com>
2026-08-27 11:20:16 +0900

    Feat: 기본 구성

--- dags/10_ctas_gold_data.py
+++ dags/10_ctas_gold_data.py
@@ -1,10 +1,10 @@
 # Silver -> Gold (CTAS 방식으로 구성)
 
 # 1. 모듈 가져오기
-from datetime import datetime
+from datetime import timedelta
 import pendulum
 from airflow import DAG
-from airflow.providers.amazon.aws.operators.athena import AthenaOpereator
+from airflow.providers.amazon.aws.operators.athena import AthenaOperator
 from airflow.providers.amazon.aws.operators.s3 import S3DeleteObjectsOperator
 
 # 2. 환경변수
@@ -22,12 +22,37 @@ GOLD_TABLE_NAME = "gold_daily_report_ctas_tbl"
 # Athena SQL 실행 결과 저장 -> 직접 지정 or 작업 그룹 지정 -> 저장되는 위치가 결정
 QUERY_RESULT_S3 = f"s3://${BUCKET_NAME}/athena/dags"
 
-# CTAS가 실제로 참조하는 데이터 저장위치
+# CTAS가 실제로 참조하는 데이터 저장위치 -> parquet 저장
 GOLD_PREFIX = "gold/daily_report_ctas/"
 GOLD_LOCATION = f"s3://{BUCKET_NAME}/{GOLD_PREFIX}"
 
-# 3. DAG 정의
+# 처리대상 날짜, 시간 등 세팅 (yyyy:mm:dd hh:mm:ss)
+TARGET_DATE = "{{dag_run.conf.get('target_date', ds)}}"
+TARGET_YEAR = "2026" # "{{dag_run.conf.get('target_year', ds)[0:4]}}"
+TARGET_MONTH = "08" # "{{dag_run.conf.get('target_month', ds)[5:7]}}"
+TARGET_DAY = "26" # "{{dag_run.conf.get('target_day', ds)[8:10]}}"
 
+# 3. DAG 정의
+with DAG( 
+  dag_id      = "10_ctas_gold_data",
+  description = "Silver -> DAG + Athena -> Gold, parquet 생성",
+  default_args= {
+    "owner"           : "aic-de1-admin",    
+    "retries"         : 1,
+    "retry_delay"     : timedelta(minutes=1)
+  },
+  schedule_interval = "0 5 * * *", # 00시 05분 00초에 참고용
+  start_date  = pendulum.datetime( 2026,6,29, tz=pendulum.timezone("Asia/Seoul") ),
+  catchup     = False,
+  tags        = ['aws', 'athena', 'ctas']
+) as dag:
+    
     # 4. TASK 정의(오퍼레이터 사용)
+    # 4-1. 기존 CTAS Gold 테이블 삭제
+    t1_drop_gold_table = AthenaOperator()
+    # 4-2. 기존 CTAS s3 데이터 삭제
+    t2_delete_gold_s3 = S3DeleteObjectsOperator()
+    # 4-3. CTAS 실행 (Silver sql 수행 -> 결과 -> 테이블 구성 -> 결과 데이터는 parquet 저장)
+    t3_create_gold_table_with_ctas = AthenaOperator()
 
     # 5. 의존성(수행 순서 >>)
