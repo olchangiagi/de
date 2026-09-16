75beabb45c8208e15a6d7b1959ec3de4d021fae1
olchangiagi <olchangiagi@gmail.com>
2026-09-16 14:12:34 +0900

    Feat: 오퍼레이터 구성

--- dags/12_emr_spark.py
+++ dags/12_emr_spark.py
@@ -1,13 +1,57 @@
 # 1. 모듈 가져오기
+from datetime import timedelta
+import pendulum
+from airflow import DAG
+# 오퍼레이터
+from airflow.operators.python import PythonOperator
+from airflow.providers.amazon.aws.operators.emr import EmrCreateJobFlowOperator, EmrAddStepsOperator, EmrTerminateJobFlowOperator
+from airflow.providers.amazon.aws.sensors.emr import EmrStepSensor
 
 # 2. 환경 변수
 
 # 3. 인프라 설정
 
 # 4. 콜백 함수
+def dummy_task_cb():
+    print("클러스터 생성 완료")
 
 # 5. DAG 정의
-
+with DAG(
+    dag_id = "12_emr_spark",
+    description = "대용량 데이터를 분산환경에서 ETML 하기 위해 스파크 사용",
+    default_args = {
+        "owner"     : 'aic-del-admin',
+        "retries"   : 1,
+        "retry_delay" : timedelta(minutes = 1)
+    },
+    schedule_interval = "daily",
+    start_date = pendulum.datetime(2026,6,29, tz = pendulum.timezone("Asia/Seoul")),
+    catchup = False,
+    tags = ['aws', 'spark', 'emr']
+) as dag:
     # 6. task 구성
+    create_cluster_task = EmrCreateJobFlowOperator( # EMR 클러스터 생성 (스파크 구동을 위한 인프라 구성)
+        task_id = "create_cluster",
+        # 인프라 구성 dict로 묘사 == Terraform의 resource "aws_emr_cluster {}"
+        job_flow_overrides = JOB_FLOW_OVERRIDES,
+        # aws 연결 정보
+        aws_conn_id = "aws_default"
+        # 인프라 구성 후 클러스터를 참조할 일이 있는 라소스 id를 자동 반환
+    )
+    dummy_task = PythonOperator( # 더미 작업, 인프라 구성 완료됨을 확인, 생략 가능
+        task_id = "dummy",
+        python_callable = _dummy_task_cb
+    )
+    run_spark_task = EmrAddStepsOperator( # 스파크 코드 작동 ETL 처리
+        task_id = "run_spark"
+    )
+    watch_spark_task = EmrStepSensor( # 센서를 통해 스파크 작업 완료 여부 확인
+        task_id = "watch_spark"
+    )
+    terminate_cluster_task = EmrTerminateJobFlowOperator( # EMR 클러스터 해제
+        task_id = "terminate_cluster"
+    )
 
     # 7. 의존성
+    create_cluster_task >> dummy_task >> run_spark_task >> watch_spark_task >> terminate_cluster_task
+    pass
